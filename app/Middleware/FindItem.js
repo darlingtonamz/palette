c83cbb5953d5895ac2ebeef117ed9315cc27ab39
Amanze Ogbonna <darlingtonamz@gmail.com>
2018-10-19 17:13:23 +0100

    Remove commented code in FindItem.js

--- app/Middleware/FindItem.js
+++ app/Middleware/FindItem.js
@@ -1,10 +1,9 @@
 'use strict'
 class FindItem {
   async handle (ctx, next, schemes) {
-    // { request, response, params: {id} }
     const { request, response } = ctx
     const id = ctx.params[schemes[1] || 'id']
-    // schemes = [ModelName, parentId]
+
     const Model = use(`App/Models/${schemes[0]}`)
     const model = await Model.find(id)
     if (!(model && model.id)) {
@@ -13,21 +12,12 @@ class FindItem {
         id
       })
     }
-    // debugger
 
     if (schemes[1]) {
       request.body.parentModel = model
-      // request.body = {parentModel: model}
-    console.log('Data 1', request.body)
-
-      // debugger
     } else {
-      // request.body = {sd: "SdSD"}
       request.body = {model}
-    // console.log('Data 2', request.body)
-    // debugger
     }
-    // console.log('Data', request.body)
 
     await next()
   }
